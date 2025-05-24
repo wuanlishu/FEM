921c7b39e7be40b7de31c10be07f15ad046fc167
wuanlishu <1140250859@qq.com>
2025-05-24 15:31:37 +0800

    update triangle element stiffness matrix

--- include/Element/TriElement.h
+++ include/Element/TriElement.h
@@ -3,21 +3,28 @@
 #define _TRI_ELEMENT_H
 
 #include "Element.h"
+#include <Eigen/Dense>
 
 class TriElement : public Element {
 public:
-    TriElement(int triEid, const std::vector<std::shared_ptr<Node>>& nodes, std::shared_ptr<Material> mat)
-    : Element(triEid, ElementType::TRI3, nodes, mat) {}
+    TriElement(int triEid, const std::vector<std::shared_ptr<Node>>& nodes, std::shared_ptr<Material> mat, double thick = 1.0)
+    : Element(triEid, ElementType::TRI3, nodes, mat), thickness(thick) {}
 
-    std::vector<std::vector<double>> computeStiffnessMatrix() const override;
-
-    std::vector<std::vector<double>> computeMassMatrix() const override;
-
+    ////////// override //////////// 
+    Eigen::MatrixXd computeStiffnessMatrix() const override;
+    Eigen::MatrixXd computeMassMatrix() const override;
     std::vector<double> computeStrain(const std::vector<double>& displacements) const override;
-
     std::vector<double> computeStress(const std::vector<double>& displacements) const override;
-
     double computeVolume() const override;
+
+public:
+    Eigen::Vector3d computeElasticMatrix() const; // [D]
+    Eigen::Vector3d computeNormalVector() const;
+    Eigen::Matrix3d computeLocalCoordSystem() const;
+    Eigen::Matrix<double, 3, 6> computeStrainDisplacementMatrix() const; // [B]
+
+private:
+    double thickness;
 };
 
 #endif
